a4d74e23f749f0362f2181ec9401154b4d48a7f7
Antonio Lobato <aj.lobato@gmail.com>
2022-06-29 05:10:23 +0000

    pretty logging

--- cmd/muddy/muddy.go
+++ cmd/muddy/muddy.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/Cidan/muddy/server"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/thejerf/suture/v4"
 )
 
 func main() {
+	// Enable pretty logging while in dev.
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	sup := suture.New("muddy", suture.Spec{
 		PassThroughPanics: true,
 	})
