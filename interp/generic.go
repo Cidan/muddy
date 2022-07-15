ce3928fd7fc52804c634e557ce41b0226b63050f
Antonio Lobato <aj.lobato@gmail.com>
2022-07-15 04:51:22 +0000

    Removed dead code

--- interp/generic.go
+++ interp/generic.go
@@ -30,27 +30,3 @@ func (g *GenericInterp) Process(ctx context.Context, player Player, command stri
 func (g *GenericInterp) Register(c *Command) {
 	g.commands[c.name] = c
 }
-
-/*
-
-type Interp struct {
-	commands map[string]*CommandRef
-}
-
-func newInterp() *Interp {
-	return &Interp{
-		commands: make(map[string]*CommandRef),
-	}
-}
-
-func (i *Interp) Register(r *CommandRef) {
-	i.commands[r.name] = r
-}
-
-func (i *Interp) Process(ctx context.Context, player Player, command string, input ...string) error {
-	if i.commands[command] != nil {
-		return i.commands[command].fn(ctx, player, input...)
-	}
-	return ErrNoCommand
-}
-*/
